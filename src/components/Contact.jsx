484479646045deeb3f433845adf0d39b9a2982f4
Marek Berta <marek.berta@accenture.com>
2021-08-11 17:32:12 +0200

    tinkering with map in contacts - adjusting footer position

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -9,8 +9,8 @@ export default function Contact() {
         // <div className="registration form">
         //     <button onClick={handleAlarm}>Fire alarm</button>
         // </div>
-        <section class="mapSection" >
+        <div class="mapSection" >
             <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d231578.0842672364!2d21.093979725198377!3d48.69452260964403!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x473ee01b67c6957b%3A0x400f7d1c6978bd0!2zS2_FoWljZQ!5e0!3m2!1ssk!2ssk!4v1628154798594!5m2!1ssk!2ssk" width="600" height="400" style={{border:0}} allowfullscreen="" loading="lazy"></iframe>
-        </section>
+        </div>
     )
 }
